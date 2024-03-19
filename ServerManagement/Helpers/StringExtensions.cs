1189646ecdc908225c01f4263fcc7da345600c89
Shaun Barnes <shaun.w.barnes@gmail.com>
2024-03-19 11:53:05 -0400

    Added server name id to the ServerComponent

---
+++ ServerManagement/Helpers/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace ServerManagement.Helpers;
+
+public static class StringExtensions
+{
+	public static string ReplaceWhiteSpaceWithDashes(this string s)
+	{
+		StringBuilder sb = new StringBuilder(s.Trim().ToLowerInvariant());
+		sb.Replace(" ", "-");
+		return sb.ToString();
+	}
+}
